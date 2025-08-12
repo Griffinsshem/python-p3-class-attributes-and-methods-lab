033d90765687714cc71d37e6c853e149f9f8122a
Griffins Shem Ondeyo <griffinsshem254@gmail.com>
2025-08-12 22:49:33 +0300

    Implement class attributes to track song counts, genres, and artists

--- lib/song.py
+++ lib/song.py
@@ -1,2 +1,28 @@
 class Song:
-    pass
+
+    count = 0
+    genres = []
+    artists = []
+    genre_count = {}
+    artist_count = {}
+
+    def __init__(self, name, artist, genre):
+        self.name = name
+        self.artist = artist
+        self.genre = genre
+
+        Song.count += 1
+        Song.genres.append(genre)
+        Song.artists.append(artist)
+
+        # update genre count
+        if genre in Song.genre_count:
+            Song.genre_count[genre] += 1
+        else:
+            Song.genre_count[genre] = 1
+
+        # update artist count
+        if artist in Song.artist_count:
+            Song.artist_count[artist] += 1
+        else:
+            Song.artist_count[artist] = 1
